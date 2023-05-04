adbfa26dcbc4cfc4101dcac5cf0513bca7942be9
Sohal <tosohal@gmail.com>
2023-05-04 12:33:54 +0200

    CAN works
    This commit:
    * removes all hardware filters from CAN
    * initializes CAN notifications for all possible CAN internal interrupts
    CANReport can send 2 bytes (int16) data on address 3 which can be interpreted as speed

--- demo/demo.c
+++ demo/demo.c
@@ -1,16 +1,39 @@
 #include <stdbool.h>
 #include <stdint.h>
+#include <string.h>
 
 #include "demo.h"
 #include "main.h"
 #include "stm32g4xx_hal.h"
 #include "stm32g4xx_hal_fdcan.h"
+#include "mc_api.h"
+
 extern FDCAN_HandleTypeDef hfdcan1;
 extern TIM_HandleTypeDef htim6;
+
+/*
+ * Data and Header for HCAN traffic
+ */
 uint8_t RxData [64];
 uint8_t TxData [64];
 FDCAN_RxHeaderTypeDef RxHeader;
 FDCAN_TxHeaderTypeDef TxHeader;
+
+/*
+ * Some motor controlling type defines
+ */
+typedef int16_t rpm;
+typedef union _rpm_i16CAN
+{
+	uint8_t bytes[2];
+	rpm rpm_i16CAN;
+}t_rpm_i16CAN;
+t_rpm_i16CAN setRPM;
+/*
+ * SAFE Value for RPM is Zero (no drive movement)
+ */
+const rpm SAFE_RPM = 0;
+
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
 	static uint32_t cnt;
@@ -39,42 +62,74 @@ void HAL_FDCAN_RxFifo0Callback(FDCAN_HandleTypeDef *hfdcan, uint32_t RxFifo0ITs)
 		{
 			Error_Handler();
 		}
+		do
+		{
+			if(RxHeader.Identifier != 0x111)
+			{
+				break;
+			}
+			if(RxHeader.DataLength != FDCAN_DLC_BYTES_2)
+			{
+				break;
+			}
+			memcpy(&setRPM.bytes[0], &RxData[0], sizeof(t_rpm_i16CAN));
+		}while (false);
 	}
-	HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
 }
 __NO_RETURN void app_main(void)
 {
-
-	FDCAN_FilterTypeDef sFilterConfig;
-    HAL_TIM_Base_Start_IT(&htim6);
-    sFilterConfig.FilterConfig = FDCAN_FILTER_TO_RXFIFO0;
-    sFilterConfig.FilterType = FDCAN_FILTER_MASK;
-    sFilterConfig.IdType = FDCAN_STANDARD_ID;
-    sFilterConfig.FilterIndex = 0;
-    sFilterConfig.FilterID1 = 0x111;
-    sFilterConfig.FilterID2 = 0;
-    if(HAL_FDCAN_ConfigFilter(&hfdcan1, &sFilterConfig) != HAL_OK)
-    {
-    	Error_Handler();
+	/* FDCan control logic for CANOpen compatiblity : No hardware filters, software filters only */
+    if (HAL_FDCAN_ConfigGlobalFilter(&hfdcan1, FDCAN_ACCEPT_IN_RX_FIFO0, FDCAN_REJECT,
+                                     FDCAN_FILTER_REMOTE, FDCAN_FILTER_REMOTE) != HAL_OK) {
+        Error_Handler();
     }
-    if (HAL_FDCAN_ConfigGlobalFilter(&hfdcan1, FDCAN_REJECT, FDCAN_REJECT, FDCAN_FILTER_REMOTE, FDCAN_FILTER_REMOTE) != HAL_OK)
+    if (HAL_FDCAN_ActivateNotification(&hfdcan1,
+            0 | FDCAN_IT_RX_FIFO0_NEW_MESSAGE | FDCAN_IT_RX_FIFO1_NEW_MESSAGE
+                | FDCAN_IT_TX_COMPLETE | FDCAN_IT_TX_FIFO_EMPTY | FDCAN_IT_BUS_OFF
+                | FDCAN_IT_ARB_PROTOCOL_ERROR | FDCAN_IT_DATA_PROTOCOL_ERROR
+                | FDCAN_IT_ERROR_PASSIVE | FDCAN_IT_ERROR_WARNING,
+            0xFFFFFFFF) != HAL_OK)
     {
-    	Error_Handler();
+        Error_Handler();
     }
-
-    if(HAL_FDCAN_ActivateNotification(&hfdcan1, FDCAN_IT_RX_FIFO0_NEW_MESSAGE, 0) != HAL_OK)
-    {
-    	Error_Handler();
-    }
-
     if(HAL_FDCAN_Start(&hfdcan1) != HAL_OK)
     {
     	Error_Handler();
     }
 
 	HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
-    do
+	HAL_TIM_Base_Start_IT(&htim6);
+	do
     {
-        HAL_Delay(20);                              /* busy wait delay */
+		rpm setRPM_i16Drive = SAFE_RPM;
+		bool maxRPM_OK = false;
+		bool minRPM_OK = false;
+		if(setRPM.rpm_i16CAN < 15000 && setRPM.rpm_i16CAN > -15000)
+		{
+			maxRPM_OK = true;
+		}
+		if(setRPM.rpm_i16CAN > 3000 || setRPM.rpm_i16CAN < -3000)
+		{
+			minRPM_OK = true;
+		}
+		if(minRPM_OK && maxRPM_OK)
+		{
+			setRPM_i16Drive = setRPM.rpm_i16CAN;
+		}
+		/**
+		 * Write a code to drive the motor with drive speed in integer
+		 */
+#if 0
+		if(setRPM_i16Drive != SAFE_RPM)
+		{
+			MC_ProgramSpeedRampMotor1((int16_t)setRPM_i16Drive, 0);
+			MC_StartMotor1();
+		}
+		else
+		{
+			MC_StopMotor1();
+		}
+#endif
+        HAL_Delay(10);                              /* busy wait delay */
     } while (true);
 }
